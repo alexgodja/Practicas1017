6fd5a5f1b8a5ae8b89511a225395be4ca071efcf
Alexandru Godja Lepadat <al435145@uji.es>
2025-02-18 12:20:53 +0100

    push

--- src/main/java/es/uji/alexandru/Row.java
+++ src/main/java/es/uji/alexandru/Row.java
@@ -1,4 +1,17 @@
 package es.uji.alexandru;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Row {
+    private List<Double> data;
+        public Row(List<Double> data){
+            this.data=new ArrayList<>(data);
+        }
+
+    public List<Double> getData(){
+        return new ArrayList<>(data);
+    }
 }
