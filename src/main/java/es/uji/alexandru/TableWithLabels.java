fe743dabe2e2a3070cf3867dfbf5403afab58349
Alex <al450630@uji.es>
2025-02-27 11:18:13 +0100

    clases y tests

--- src/main/java/es/uji/alexandru/TableWithLabels.java
+++
@@ -1,9 +0,0 @@
-package es.uji.alexandru;
-
-import java.util.List;
-
-public class TableWithLabels extends Table {
-    public TableWithLabels(List<String> headers) {
-        super(headers);
-    }
-}
---
+++ src/main/java/es/uji/alexandru/table/RowWithLabel.java
@@ -0,0 +1,9 @@
+package es.uji.alexandru.table;
+
+public class RowWithLabel {
+    private String label;
+
+    public String getLabel() {
+        return label;
+    }
+}
