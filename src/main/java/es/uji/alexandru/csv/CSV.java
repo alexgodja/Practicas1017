9395e15a64488ea5a4d0996bf3131820791aa4b3
doaga <doaga@alumni.uv.es>
2025-02-28 00:15:09 +0100

    CSV readTableWithLabels

--- src/main/java/es/uji/alexandru/csv/CSV.java
+++ src/main/java/es/uji/alexandru/csv/CSV.java
@@ -1,6 +1,7 @@
 package es.uji.alexandru.csv;
 
 import es.uji.alexandru.table.Row;
+import es.uji.alexandru.table.RowWithLabel;
 import es.uji.alexandru.table.Table;
 import es.uji.alexandru.table.TableWithLabels;
 
@@ -8,9 +9,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class CSV {
 
@@ -43,6 +42,34 @@ public class CSV {
         return new Table (headers, rows);
     }
     public TableWithLabels readTableWithLabels(String ruta) throws IOException {
-        return null;
+        List<String> headers = new ArrayList<>();
+        List<Row> rows = new ArrayList<>();
+        Map<String,Integer> labelsToIndex = new HashMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(ruta))){
+            String linea = reader.readLine();
+            if(linea!=null){
+                String[] cabeceras = linea.split(",");
+                for (String cabecera : cabeceras) {
+                    headers.add(cabecera.trim());
+                }
+            }
+            while ((linea = reader.readLine()) != null) {
+                String[] valores = linea.split(",");
+                List<Double> valoresDouble = new ArrayList<>();
+                String etiqueta = valores[valores.length - 1].trim();
+
+                for (String valor : valores) {
+                    valoresDouble.add(Double.parseDouble(valor.trim()));
+                }
+
+                if (!labelsToIndex.containsKey(etiqueta)) {
+                    labelsToIndex.put(etiqueta, labelsToIndex.size());
+                }
+
+                rows.add(new RowWithLabel(valoresDouble,etiqueta));
+            }
+            return new TableWithLabels(headers, rows,labelsToIndex);
+        }
     }
 }
--- src/main/java/es/uji/alexandru/table/TableWithLabels.java
+++ src/main/java/es/uji/alexandru/table/TableWithLabels.java
@@ -1,18 +1,23 @@
 package es.uji.alexandru.table;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TableWithLabels extends Table {
 
+    Map<String,Integer> labelsToIndex = new HashMap<String,Integer>();
+
     //Constructor
-    public TableWithLabels(List<String> headers, List<Row> rows) {
+    public TableWithLabels(List<String> headers, List<Row> rows, Map<String, Integer> labelsToIndex) {
         super(headers, rows);
+        this.labelsToIndex = labelsToIndex;
     }
 
     //Devuelve la fila del número que se pasa
     @Override
     public RowWithLabel getRowAt(int numFila) {
-        
+        return null;
     }
 
 
