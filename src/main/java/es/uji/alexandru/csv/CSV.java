0d95710ff2630b5915362ebdfbedb99f679610a3
doaga <doaga@alumni.uv.es>
2025-02-28 23:54:12 +0100

    Clase CSV

--- src/main/java/es/uji/alexandru/csv/CSV.java
+++ src/main/java/es/uji/alexandru/csv/CSV.java
@@ -22,56 +22,63 @@ public class CSV {
         //Obtener ruta
         String ruta = getClass().getClassLoader().getResource(archivo).toURI().getPath();
 
+        //Leer archivo
         try (BufferedReader reader = new BufferedReader(new FileReader(ruta))){
+            //Leer primera línea con cabeceras y añadirla a headers
             String linea = reader.readLine();
             if(linea != null){
                 String[] cabeceras = linea.split(",");
                 for (String cabecera : cabeceras) {
-                    headers.add(cabecera.trim());
+                    headers.add(cabecera);
                 }
             }
+            //Leer filas con valores y añadirlas a rows
             while ((linea = reader.readLine()) != null) {
                 String[] valores = linea.split(",");
                 List<Double> valoresDouble = new ArrayList<>();
                 for (String valor : valores) {
-                    valoresDouble.add(Double.parseDouble(valor.trim()));
+                    valoresDouble.add(Double.parseDouble(valor));
                 }
                 rows.add(new Row(valoresDouble));
 
             }
         }
+
         return new Table (headers, rows);
     }
     public TableWithLabels readTableWithLabels(String archivo) throws IOException, URISyntaxException {
         List<String> headers = new ArrayList<>();
-        List<RowWithLabel> rows = new ArrayList<>();
+        List<RowWithLabel> rowsWithLabels = new ArrayList<>();
 
         //Obtener ruta
         String ruta = getClass().getClassLoader().getResource(archivo).toURI().getPath();
 
+        //Leer archivo
         try (BufferedReader reader = new BufferedReader(new FileReader(ruta))) {
+            //Leer primera línea con cabeceras y añadirla a headers
             String linea = reader.readLine();
             if (linea != null) {
                 String[] cabeceras = linea.split(",");
 
                 for (int i = 0; i < cabeceras.length - 1; i++) {
-                    headers.add(cabeceras[i].trim());
+                    headers.add(cabeceras[i]);
                 }
             }
 
+            //Leer filas con valores y etiquetas y añadirlas a rowsWithLabels
             while ((linea = reader.readLine()) != null) {
                 String[] valores = linea.split(",");
                 List<Double> valoresDouble = new ArrayList<>();
                 String etiqueta = valores[valores.length - 1].trim();
 
                 for (int i = 0; i < valores.length - 1; i++) {
-                    valoresDouble.add(Double.parseDouble(valores[i].trim()));
+                    valoresDouble.add(Double.parseDouble(valores[i]));
                 }
 
-                rows.add(new RowWithLabel(valoresDouble, etiqueta));
+                rowsWithLabels.add(new RowWithLabel(valoresDouble, etiqueta));
             }
         }
 
-        return new TableWithLabels(headers, rows);
+        return new TableWithLabels(headers, rowsWithLabels);
     }
 }
