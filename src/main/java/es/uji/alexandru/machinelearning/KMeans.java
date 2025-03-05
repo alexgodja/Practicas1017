2db4b6feb08616a8d592c173b28d1e33b14151f1
doaga <doaga@alumni.uv.es>
2025-03-05 22:11:32 +0100

    clase KMeans y interfaz Algorithm

--- src/main/java/es/uji/alexandru/machinelearning/KMeans.java
+++ src/main/java/es/uji/alexandru/machinelearning/KMeans.java
@@ -1,4 +1,17 @@
 package es.uji.alexandru.machinelearning;
 
-public class KMeans {
+import es.uji.alexandru.table.Table;
+import java.util.List;
+
+public class KMeans implements Algorithm <Table,Integer,List<Double>> {
+
+    @Override
+    public void train(Table data) {
+
+    }
+
+    @Override
+    public Integer estimate(List<Double> sample) {
+        return null;
+    }
 }
--- src/main/java/es/uji/alexandru/machinelearning/KNN.java
+++ src/main/java/es/uji/alexandru/machinelearning/KNN.java
@@ -5,7 +5,7 @@ import es.uji.alexandru.table.TableWithLabels;
 
 import java.util.List;
 
-public class KNN {
+public class KNN implements  Algorithm<TableWithLabels,Integer,List<Double>> {
     private TableWithLabels trainingData;
     public void train(TableWithLabels iris) {
         this.trainingData=iris;
