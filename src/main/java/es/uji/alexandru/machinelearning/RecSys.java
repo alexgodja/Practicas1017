c6f56126538980f5c4d72b387f9764d9bce70dec
Alexandru Godja Lepadat <al435145@uji.es>
2025-03-18 11:11:08 +0100

    hola

---
+++ src/main/java/es/uji/alexandru/machinelearning/RecSys.java
@@ -0,0 +1,20 @@
+package es.uji.alexandru.machinelearning;
+
+import es.uji.alexandru.table.Table;
+
+import java.util.List;
+
+public class RecSys {
+    public RecSys(Algorithm algorithm){
+
+    }
+    public void train (Table trainData){
+
+    }
+    public void initialise(Table testData, List<String> testItemNames){
+
+    }
+    public List<String> recommend(String nameLikedItem, int numRecommendations){
+
+    }
+}
