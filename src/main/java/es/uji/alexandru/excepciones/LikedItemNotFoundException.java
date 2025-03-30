c563b1d4635f539092b4bbbc67db5cbabdbcb8c1
Alexandru Godja Lepadat <al435145@uji.es>
2025-03-30 16:13:50 +0200

    RecSys+ excepciones

--- src/main/java/es/uji/alexandru/excepciones/LikedItemNotFoundException.java
+++ src/main/java/es/uji/alexandru/excepciones/LikedItemNotFoundException.java
@@ -1,4 +1,11 @@
 package es.uji.alexandru.excepciones;
 
-public class LikedItemNotFoundException {
+public class LikedItemNotFoundException extends RuntimeException{
+    private String item;
+    public LikedItemNotFoundException(String item){
+        this.item=item;
+    }
+    public String getItem(){
+        return item;
+    }
 }
--- src/main/java/es/uji/alexandru/recommender/RecSys.java
+++ src/main/java/es/uji/alexandru/recommender/RecSys.java
@@ -2,21 +2,49 @@ package es.uji.alexandru.recommender;
 
 import es.uji.alexandru.algorithms.Algorithm;
 import es.uji.alexandru.data.table.Table;
+import es.uji.alexandru.excepciones.LikedItemNotFoundException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RecSys{
     private Algorithm algorithm;
+    private Table trainData;
+    private Table testData;
+    private List<String> testItemNames;
+    private Map<String,String> mapa;
     public RecSys(Algorithm algorithm){
         this.algorithm=algorithm;
+        this.mapa=new HashMap<>();
     }
     public void train (Table trainData){
-
+        this.trainData=trainData;
+        algorithm.train(trainData);
     }
     public void initialise(Table testData, List<String> testItemNames){
+        this.testData=testData;
+        this.testItemNames=testItemNames;
+        for (int i=0;i<testData.getRowCount();i++){
+            List<Double> valores=testData.getRowAt(i).getData();
+            String estimacion= String.valueOf(algorithm.estimate(valores));
+            mapa.put(testItemNames.get(i),estimacion);
+        }
 
     }
     public List<String> recommend(String nameLikedItem, int numRecommendations){
-        return null;
+        if (!mapa.containsKey(nameLikedItem)){
+            throw new LikedItemNotFoundException(nameLikedItem);
+        }
+        String likes=mapa.get(nameLikedItem);
+        List<String> recomendaciones=new ArrayList<>();
+        for (String item : testItemNames){
+            if (recomendaciones.size()>=numRecommendations)
+                break;
+            if (!item.equals(nameLikedItem) && mapa.get(item).equals(likes))
+                recomendaciones.add(item);
+        }
+        return recomendaciones;
     }
 }
