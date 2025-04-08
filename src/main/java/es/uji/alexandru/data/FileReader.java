e4a5e3cdc534d03d596d94f17a8fb901eef2cd1c
Alex <al450630@uji.es>
2025-04-09 01:16:04 +0200

    si

---
+++ src/main/java/es/uji/alexandru/data/FileReader.java
@@ -0,0 +1,43 @@
+package es.uji.alexandru.data;
+
+import es.uji.alexandru.data.table.Table;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.util.Scanner;
+
+public class FileReader <T extends Table> extends ReaderTemplate  {
+    Scanner sc;
+
+    public FileReader(String source) {
+        super(source);
+    }
+
+    @Override
+    void openSource(String source) throws URISyntaxException {
+        try
+        {
+            String ruta = getClass().getClassLoader().getResource(source).toURI().getPath();
+            sc = new Scanner(new File(ruta));
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException("No se ha encontrado el archivo ",e);
+        }
+    }
+
+    @Override
+    void closeSource() {
+        sc.close();
+    }
+
+    @Override
+    boolean hasMoreData() {
+
+    }
+
+    @Override
+    String getNextData() {
+        return ;
+    }
+}
--- src/main/java/es/uji/alexandru/data/ReaderTemplate.java
+++ src/main/java/es/uji/alexandru/data/ReaderTemplate.java
@@ -2,16 +2,20 @@ package es.uji.alexandru.data;
 
 import es.uji.alexandru.data.table.Table;
 
+import java.net.URISyntaxException;
+
 abstract class ReaderTemplate < T extends Table> {
 
     protected String source;
+    protected String headers;
+    protected String data;
     protected T table;
 
-    public ReaderTemplate(){
-        this.source
+    public ReaderTemplate(String source) {
+        this.source = source;
     }
 
-    abstract void openSource (String source);
+    abstract void openSource (String source) throws URISyntaxException;
     abstract void processHeaders (String headers);
     abstract void processData (String data);
     abstract void closeSource ();
@@ -20,7 +24,11 @@ abstract class ReaderTemplate < T extends Table> {
 
     public final T readTableFromSource(){
         openSource(source);
-
+        processHeaders(headers);
+        processData(data);
+        closeSource();
+        hasMoreData();
+        getNextData();
 
         return table;
     }
