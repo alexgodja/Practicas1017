c76fbfdc12f4c51ba7f0b9d44107e2145b522058
Alexandru Godja Lepadat <al435145@uji.es>
2025-04-24 23:24:53 +0200

    javafx

--- src/main/java/es/uji/alexandru/MainApp.java
+++ src/main/java/es/uji/alexandru/MainApp.java
@@ -1,4 +1,24 @@
 package es.uji.alexandru;
 
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import java.awt.*;
+
 public class MainApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox layout =new VBox();
+        Label label=new Label("Hola");
+        Button button=new Button("Diego es gay xd");
+        layout.getChildren().addAll(label,button);
+        layout.setAlignment(Pos.CENTER);
+        primaryStage.setScene(new Scene(layout,200,100));
+        primaryStage.setTitle("Recomendador");
+        primaryStage.show();
+    }
 }
