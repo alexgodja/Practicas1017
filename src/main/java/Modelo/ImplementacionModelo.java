0f5ba46d815be25398e04c4f6e45a7dac8d3dd05
doaga <doaga@alumni.uv.es>
2025-04-29 16:38:31 +0200

    Estructura

--- src/main/java/Modelo/ImplementacionModelo.java
+++ src/main/java/Modelo/ImplementacionModelo.java
@@ -3,9 +3,10 @@ package Modelo;
 import Vista.ImplementacionVista;
 import Vista.InformaVista;
 
-public class ImplementacionModelo implements CambioModelo,
-        InterrogaModelo {
+public class ImplementacionModelo implements CambioModelo, InterrogaModelo {
     private InformaVista vista;
+
+    public ImplementacionModelo(){}
 
     public void setVista(InformaVista vista) {
         this.vista = vista;
